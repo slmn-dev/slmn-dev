a14709f34762e34f945002bd730ebf937473ac00
Slmn <slmnlfrs@MacBook-Pro-Slmn.local>
2025-09-13 23:05:00 +0700

    fresh commit for vercel deployment again

--- api/index.php
+++ api/index.php
@@ -1,17 +1,20 @@
 <?php
-use Illuminate\Contracts\Http\Kernel;
+
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-require_once __DIR__.'/../vendor/autoload.php';
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
