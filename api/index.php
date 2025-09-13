c6afb3a3bc725ed9075b4d86c26f9f6a7e0ecaa8
Slmn <slmnlfrs@MacBook-Pro-Slmn.local>
2025-09-13 23:22:17 +0700

    solve index.php

--- api/index.php
+++ api/index.php
@@ -1,20 +1,19 @@
 <?php
-
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+// Pastikan path autoload benar
+require_once __DIR__.'/../vendor/autoload.php';
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// Pastikan path bootstrap benar  
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
